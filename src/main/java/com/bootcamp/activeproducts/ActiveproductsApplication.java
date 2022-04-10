f220c8ec41bc7504d3393f8cfc659a9c2a317c10
Juan Jose Alejo <juanjose.alejofalcon@nttdata.com>
2022-04-10 14:06:25 -0500

    Microservicio para Productos Activos

---
+++ src/main/java/com/bootcamp/activeproducts/ActiveproductsApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.activeproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveproductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveproductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/activeproducts/ActiveproductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.activeproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveproductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
