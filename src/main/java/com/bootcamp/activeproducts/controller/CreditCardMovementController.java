e37c20222ad60e3cc9dd247500f8094a00c688a0
Juan Jose Alejo <juanjose.alejofalcon@nttdata.com>
2022-04-13 15:33:37 -0500

    Update - Reglas de Negocio

--- src/main/java/com/bootcamp/activeproducts/controller/CreditCardMovementController.java
+++ src/main/java/com/bootcamp/activeproducts/controller/CreditCardMovementController.java
@@ -3,7 +3,10 @@ package com.bootcamp.activeproducts.controller;
 import com.bootcamp.activeproducts.model.CreditCardMovement;
 import com.bootcamp.activeproducts.service.CreditCardMovementService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -11,6 +14,9 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/creditCardMovement")
 @RequiredArgsConstructor
 public class CreditCardMovementController {
+    @Value("${passiveproducts.server.url}")
+    private String actPrdUrl;
+
     public final CreditCardMovementService service;
     @GetMapping
     public Flux<CreditCardMovement> getAll(){
@@ -42,4 +48,19 @@ public class CreditCardMovementController {
         return service.deleteById(id);
     }
 
+    @GetMapping("/accountBalance/{account}")
+    public String getAccountBalance(@PathVariable("account") String account){
+        double balance=0;
+        RestTemplate restTemplate=new RestTemplate();
+        String urlDp = actPrdUrl +"/creditCardMovement/find/" + account;
+        ResponseEntity<CreditCardMovement[]> creditCardMovements = restTemplate.getForEntity(urlDp,CreditCardMovement[].class);
+        for(CreditCardMovement am: creditCardMovements.getBody()){
+            if (am.getMovementType().equals("D")){
+                balance += Double.parseDouble(am.getAmount());
+            }else{
+                balance -= Double.parseDouble(am.getAmount());
+            }
+        }
+        return String.valueOf(balance);
+    }
 }
